4ca9575d9e9d87f526f8907b211546efe3f3a2e9
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-02-27 23:23:25 +0500

    sm_ex_done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Button from "./components/Button.tsx";
 const App = () => {
     return (
         <div>
-
+            <Button label={'click me'} onClick={() => {
+                console.log('Hello world')}} disabled={false}/>
         </div>
     )
 }
