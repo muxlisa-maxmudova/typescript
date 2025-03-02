3bec8a9562e84afba4a9c76da929bd19a6ab2dc0
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-02 11:13:51 +0500

    counter.tsx component inside state_hooks create

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import UserInfo from "./components/UserInfo.tsx";
-import AdminInfo from "./components/AdminInfo.tsx";
-import {AdminInfoList, Info} from "./types.ts";
+import Counter from "./State_hook/Counter.tsx";
 const App = () => {
-    const user : Info = {
-        id:1,
-        name:'Joe',
-        email:'joe@gmail.com',
-    };
-    const admin : AdminInfoList = {
-        // we have to indicate the
-        // types importing from types
-        id:1,
-        name:'Jenna',
-        email:'jenna@gmail.com',
-        role:'admin',
-        lastLogin: new Date(),
-    };
+
     return (
         <div>
-            <UserInfo user={user}/>
-            <AdminInfo admin={admin}/>
+            <Counter/>
         </div>
     )
 }
---
+++ src/State_hook/Counter.tsx
@@ -0,0 +1,10 @@
+const Counter = () => {
+
+    return (
+        <div>
+            <h1>Counter</h1>
+
+        </div>
+    )
+}
+export default Counter
