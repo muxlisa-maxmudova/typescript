3a749d8528dd338319109293af0b3e9bf9e133b1
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-02 11:31:28 +0500

    counter worked

--- src/State_hook/Counter.tsx
+++ src/State_hook/Counter.tsx
@@ -1,9 +1,12 @@
+import {useState} from "react";
 const Counter = () => {
-
+    const [count, setCount] = useState<number>(0);//<number> here is type
     return (
         <div>
             <h1>Counter</h1>
-
+            <p>{count}</p>
+            <button onClick={()=>{setCount(prevState => prevState+1)}}>Increase</button>
+            <button onClick={()=>{setCount(prevState => prevState-1)}}>Decrease</button>
         </div>
     )
 }
